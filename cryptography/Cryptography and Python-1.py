3b5767fbd37864a927783323b9e392036a879231
odysseasEko <p19oiko@ionio.gr>
2022-10-22 13:58:01 +0300

    added first crypt ex

---
+++ cryptography/Cryptography and Python-1.py
@@ -0,0 +1,34 @@
+# P2019060 Οδυσσέας Οικονομου
+
+#Install libraries with pip install cryptography at terminal
+
+# Python Strings
+mathima = "Kriptografia 2022"
+lang = "python"
+
+# Python List
+specs = [22, "thessaloniki", 175, "Odysseas"]
+tinySpecs = [40, "hp"]
+
+#Python Tuples 
+myTuple = ("ionio", 4)
+
+#Python Dictionary
+musicDict = {"rap": "TRK", "vibes": "imaculate", "views": 2}
+
+#Python Calc
+print((4+3)/2*38)
+
+#Python Division RN
+print(9/2, 9.0/2, 9/2.0, 9.0/2.0, 9./2, 9/2.)
+
+#Python Pow
+print(6**3, 4**4.)
+
+#Θελουμε x+2xy+y=z
+x = -3
+y = 14
+z = 55.0
+ex = ((x + 2*x*y + y)/z)
+print(ex)
+
